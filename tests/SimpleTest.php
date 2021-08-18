e0d94170361fa6e60b0b1c5bdae2fae4eeb706ea
melireddy <59958918+melireddy@users.noreply.github.com>
2021-08-18 20:03:17 +0200

    Update SimpleTest.php

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -1,1 +1,18 @@
+<?php
 
+require __DIR__ . "/../src/Simple.php";
+require __DIR__ .'/../vendor/autoload.php';
+
+class SimpleTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testDivide()
+    {
+        $simple = new Simple(10);
+        $result = $simple->divide(2);
+
+        $this->assertEquals(5, $result);
+    }    
+}
+
+?>
