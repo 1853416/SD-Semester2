1e5ced97daa402d2bfefe22f17c5ac4779017331
melireddy <59958918+melireddy@users.noreply.github.com>
2021-09-13 20:57:55 +0200

    Delete test directory

--- test/EmailValidator.java
+++
@@ -1,5 +0,0 @@
-public class EmailValidator {
-    public static boolean isValidEmail(String email){
-        return email.contains("@") && (email.contains(".com") || email.contains(".co.za"));
-    }
-}
--- test/EmailValidatorTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.Assert;
-import org.junit.Test;
-public class EmailValidatorTest {
-    @Test
-    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-        Assert.assertTrue(EmailValidator.isValidEmail("name@email.com"));
-    }
-}
