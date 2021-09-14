9da19c4e6d8ea5a0e28a5190512eb3df5174f32c
melireddy <59958918+melireddy@users.noreply.github.com>
2021-09-14 10:18:52 +0200

    Delete src directory

--- src/main/java/org/jacoco/examples/maven/java/EmailValidator.java
+++
@@ -1,7 +0,0 @@
-package org.jacoco.examples.maven.java;
-
-public class EmailValidator {
-    public boolean isValidEmail(String email){
-           return email.contains("@") && (email.contains(".com") || email.contains(".co.za"));
-    }
-}
