084c6fbe1c076a62e599c84e2166f1510a8f285d
Bianco-VD-Ross <61656181+Bianco-VD-Ross@users.noreply.github.com>
2021-09-28 13:46:47 +0200

    Delete Sprint 2 - Website/Reworked directory

--- Sprint 2 - Website/Reworked/js/DocHome.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load',()=> {
-    const params =(new URL(document.location)).searchParams;
-    const phone =params.get('phone');
-    document.getElementById('drName').innerHTML=phone;
-    
-
-})
