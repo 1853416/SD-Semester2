1c24d857119e3a0a0e11fec1b7779cff3a7f0d9c
Xin <jackxnian@gmail.com>
2021-11-04 22:41:14 +0200

    update ui and modify email

--- app/src/main/java/com/example/patienttracker/Activity_Patient_Settings.java
+++ app/src/main/java/com/example/patienttracker/Activity_Patient_Settings.java
@@ -3,6 +3,7 @@ package com.example.patienttracker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.CalendarView;
 
@@ -10,10 +11,13 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Activity_Patient_Settings extends AppCompatActivity {
-
+    private String patient_document_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_settings);
+
+        Intent intent = getIntent();
+        patient_document_id = intent.getStringExtra(Fragment_Patient_Home.phoneKey);
     }
 }
