8ac62728c6838dc35e9bad8c623d2c3fa1e31197
Xin <jackxnian@gmail.com>
2021-11-05 07:39:13 +0200

    adding features adn polishing app

--- app/src/main/java/com/example/patienttracker/Activity_Doctor_Settings.java
+++ app/src/main/java/com/example/patienttracker/Activity_Doctor_Settings.java
@@ -2,13 +2,61 @@ package com.example.patienttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.Serializable;
 
 public class Activity_Doctor_Settings extends AppCompatActivity {
+    //string
+    public final static String doctorPhoneKEY = "DoctorPhone";
+    String doctor_documentID;
+    //widgets
+    Button btn_policy,btn_update,btn_password,btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_settings);
+
+        final Intent intent = getIntent();
+        doctor_documentID = intent.getStringExtra(Activity_Doctor_Login.doctorPhoneKEY);
+
+        btn_policy = findViewById(R.id.B_A_DoctorSetting_Policy);
+        btn_update = findViewById(R.id.B_A_DoctorSetting_Update_Info);
+        btn_password = findViewById(R.id.B_A_DoctorSetting_Change_Password);
+        btn_back = findViewById(R.id.B_A_DoctorSetting_Back);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btn_policy.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Activity_Doctor_Policy.class);
+            startActivity(intent);
+        });
+
+        btn_update.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Activity_Doctor_Update_Info.class);
+            intent.putExtra(doctorPhoneKEY,doctor_documentID);
+            startActivity(intent);
+        });
+
+        btn_password.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Activity_Doctor_Change_Password.class);
+            intent.putExtra(doctorPhoneKEY,doctor_documentID);
+            startActivity(intent);
+        });
+
+        btn_back.setOnClickListener(v -> {
+            onBackPressed();
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/patienttracker/Activity_Patient_Settings.java
+++ app/src/main/java/com/example/patienttracker/Activity_Patient_Settings.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CalendarView;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Activity_Patient_Settings extends AppCompatActivity {
-    private String patient_document_id;
+    //strings
+    String patient_document_id;
+    //widgets
+    Button btn_policy,btn_update,btn_password,btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,41 @@ public class Activity_Patient_Settings extends AppCompatActivity {
 
         Intent intent = getIntent();
         patient_document_id = intent.getStringExtra(Fragment_Patient_Home.phoneKey);
+
+        btn_policy = findViewById(R.id.B_A_PatientSetting_Policy);
+        btn_update = findViewById(R.id.B_A_PatientSetting_Update_Info);
+        btn_password = findViewById(R.id.B_A_PatientSetting_Change_Password);
+        btn_back = findViewById(R.id.B_A_PatientSetting_Back);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btn_policy.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Activity_Patient_Policy.class);
+            startActivity(intent);
+        });
+
+        btn_update.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Activity_Patient_Update_Info.class);
+            intent.putExtra(Fragment_Patient_Home.phoneKey,patient_document_id);
+            startActivity(intent);
+        });
+
+        btn_password.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Activity_Patient_Change_Password.class);
+            intent.putExtra(Fragment_Patient_Home.phoneKey,patient_document_id);
+            startActivity(intent);
+        });
+
+        btn_back.setOnClickListener(v -> {
+            onBackPressed();
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
